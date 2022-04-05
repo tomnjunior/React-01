0d97a1cbf25d5f56973420243d20599c93b887dc
tomnjunior <tomnjunior@gmail.com>
2022-04-05 16:01:23 -0300

    React-01

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './Menu'
+
+function Header(props){
+    return(
+        <header className="header">
+            <h1>{props.name}</h1>
+            <Menu links={props.links}></Menu>
+        </header>
+    );
+}
+
+
+export default Header;
